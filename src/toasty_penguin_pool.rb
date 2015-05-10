c3a6250392d87b15084db260c51a0eeccb983264
Sammidysam <sammidysam@gmail.com>
2015-05-10 10:40:20 -0400

    A window now opens

--- src/toasty_penguin_pool.rb
+++ src/toasty_penguin_pool.rb
@@ -1,3 +1,33 @@
 #! /usr/bin/env ruby
 
 require "gosu"
+
+require_relative "field"
+require_relative "penguin"
+
+class GameWindow < Gosu::Window
+	def initialize
+		super 800, 400, false
+	end
+
+	# Allows a button to be held down and still activated.
+	def button_down(id)
+	end
+
+	# Only allows the button to be pressed once for this to run.
+	def button_up(id)
+		case id
+		when Gosu::KbEscape
+			close
+		end
+	end
+
+	def update
+	end
+
+	def draw
+	end
+end
+
+window = GameWindow.new
+window.show
