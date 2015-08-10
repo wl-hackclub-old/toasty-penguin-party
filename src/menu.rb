a1ad676817e130a6d809735dfa0226d52383ded7
Sammidysam <sammidysam@gmail.com>
2015-08-10 16:29:18 -0400

    Add Menu "Press enter to begin" prompt

--- src/menu.rb
+++ src/menu.rb
@@ -11,5 +11,6 @@ class Menu < Location
 	def draw
 		@font = Gosu::Font.new(@window, Gosu::default_font_name, 20)
 		@font.draw_rel("Toasty Penguin Party", (@window.width / 2), (@window.height / 2) - 70, ZOrder::Menu, 0.5, 0.5, 2.0, 2.0, 0xffffffff)
+		@font.draw_rel("Press enter to begin", (@window.width / 2), (@window.height * 0.9), ZOrder::Menu, 0.5, 0.5, 1.3, 1.3, 0xffffffff)
 	end
 end
