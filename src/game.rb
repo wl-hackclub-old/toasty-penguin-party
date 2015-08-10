dbb5976473e921d6c02a4d70ac541badf7e57469
Sammidysam <sammidysam@gmail.com>
2015-08-10 16:47:52 -0400

    Display the current turn in the game

--- src/game.rb
+++ src/game.rb
@@ -16,14 +16,16 @@ class Game < Location
 	end
 
 	def draw
+		@font.draw_rel("It is player #{@turn + 1} turn.", 10, 20, 0.0, 0.0, 1.0, 1.0)
+
 		@penguins.each(&:draw)
 		@toasts.each(&:draw)
 	end
 
 	def increment_turn
-		turn += 1
-		if turn == players
-			turn = 0
+		@turn += 1
+		if @turn == @players
+			@turn = 0
 		end
 	end
 end
